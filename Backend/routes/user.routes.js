863e0cd230df66c247f44f30df3b544ed52d6a0c
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2025-01-31 23:54:18 +0530

    created the register route

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+const { body } = require('express-validator');
+
+//add validations routes
+//1. "register" 2. 
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('fullname.firstname').isLength({min: 3}).withMessage('First name must be at least 3 characters long'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long')
+],
+    userController.RegisterUser
+)
+
+
+
+module.exports = router;
