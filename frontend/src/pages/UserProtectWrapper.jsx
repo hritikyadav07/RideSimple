0b09a8c2a59e3d1a7619756bb9aee91a0493a221
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2025-02-10 16:37:09 +0530

    updated Protected Routes

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -1,5 +1,7 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState, useContext} from 'react'
 import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import {UserDataContext} from '../context/UserContext'
 
 
 const UserProtectWrapper = ({children}) => {
@@ -7,11 +9,36 @@ const UserProtectWrapper = ({children}) => {
   const token = localStorage.getItem('token');
   const navigate = useNavigate()
 
-  useEffect(() => {
-    if(!token){
-      navigate('/login')
+  const { user, setUser } = useContext(UserDataContext)
+    const [ isLoading, setIsLoading ] = useState(true)
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/login')
+        }
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                setUser(response.data)
+                setIsLoading(false)
+            }
+        })
+            .catch(err => {
+                console.log(err)
+                localStorage.removeItem('token')
+                navigate('/login')
+            })
+    }, [ token ])
+
+    if (isLoading) {
+        return (
+            <div>Loading...</div>
+        )
     }
-  },[token])
 
   return (
     <>
