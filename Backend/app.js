e09242897ea619be276eeafbf2d571dd79b00c56
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2025-04-12 10:26:37 +0530

    everything working fine

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,14 @@ const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
